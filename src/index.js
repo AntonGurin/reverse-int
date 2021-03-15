1904fde659a3e5dcf030fad1065dd3e6cdace677
AntonGurin <79761911+AntonGurin@users.noreply.github.com>
2021-03-15 20:47:37 +0300

    change: index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let num = n + "";
+    if (num < 0) {
+        return Math.abs(num);
+    } else if (num >= 0) {
+        return num.split("").reverse().join("");
+    } else {
+        return num;
+    }
 }
